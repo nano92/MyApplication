bc78b9b7fc5ff0d6710fa08a11834d02e93fd663
unknown <Lgallet@Pelm-1899.office.pelmorex.com>
2015-09-21 16:28:24 -0400

    updated version

--- app/src/main/java/com/example/lgallet/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lgallet/myapplication/MainActivity.java
@@ -5,13 +5,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView mainTextView;
     Button mainButton;
+    EditText mainEditText;
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
 
 
     @Override
@@ -22,11 +31,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // 1. Access the TextView defined in layout XML
         // and then set its text
         mainTextView=(TextView)findViewById(R.id.main_textview);
-        //mainTextView.setText("Set in Java!");
+        mainTextView.setText("Set in Java!");
 
         // 2. Access the Button defined in layout XML and listen for it here
         mainButton= (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
+
+        // 3. Access the EditText defined in layout XML
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
+
+        // 4. Access the ListView
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+// Create an ArrayAdapter for the ListView
+        mArrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,mNameList);
+
+// Set the ListView to use the ArrayAdapter
+        mainListView.setAdapter(mArrayAdapter);
 
     }
 
@@ -39,9 +60,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        // Take what was typed into the EditText
+        // and use in TextView
+        mainTextView.setText(mainEditText.getText().toString() + " is learning Android development!");
+
+        // Also add that value to the list shown in the ListView
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
+
         //Test the button
 
-        mainTextView.setText("Button pressed");
+       // mainTextView.setText("Button pressed");
     }
 
     /*@Override
